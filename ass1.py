3d4f7da8d2bbaaec519944bf7ce35822c9ade64b
Chemberg Dim Escurel <42348784+chemescurel@users.noreply.github.com>
2019-03-13 18:07:19 +0800

    updated

--- ass1.py
+++ ass1.py
@@ -1,8 +1,24 @@
 import requests
+from pprint import pprint
 
-api_address='http://api.openweathermap.org/data/2.5/weather?appid=5b79508949bb7ef46f90124386e740eeq='
-city = raw_input('City Name :')
-url = api_address + city
-json_data = requests.get(url).json()
-format_add = json_data['base']
-print(format_add)
+city = input('Enter your city : ')
+
+url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=5b79508949bb7ef46f90124386e740ee&units=metric'.format(city)
+
+res = requests.get(url)
+
+data = res.json()
+
+temp = data['main']['temp']
+wind_speed = data['wind']['speed']
+
+latitude = data['coord']['lat']
+longitude = data['coord']['lon']
+
+description = data['weather'][0]['description']
+
+print('Temperature : {} degree celcius'.format(temp))
+print('Wind Speed : {} m/s'.format(wind_speed))
+print('Latitude : {}'.format(latitude))
+print('Longitude : {}'.format(longitude))
+print('Description : {}'.format(description))
